fb918ff7b5eeae211c2dc2569d4278deb37e55b4
pfarkas93 <pfarkas93@gmail.com>
2024-01-16 10:43:20 +0200

    RTE setup

---
+++ src/02_RTE/RTE_Communication.h
@@ -0,0 +1,14 @@
+#ifndef _RTE_COMMUNICATION_H_
+#define _RTE_COMMUNICATION_H_
+
+#include "Types.h"
+
+/**
+ * @brief Initialize communication run time service
+ * 
+ * @return none
+ */
+void RTE_Communication_Init(void);
+
+
+#endif //_RTE_COMMUNICATION_H_
---
+++ test/02_RTE/TestRTE_Communication.c
@@ -0,0 +1,22 @@
+#include "unity.h"
+#include "RTE_Communication.h"
+
+void setUp(void)
+{
+    //mock_..._Init();
+}
+
+void tearDown(void)
+{
+    //mock_..._Verify();
+    
+    //mock_..._Destroy();
+}
+
+/*RTE_Communication_Init()************************************************************/
+void test_Given_WhenCommunicationInitCalled_ThenExpect(void)
+{
+   
+
+    RTE_Communication_Init();
+}
