2f9bbcf8dd70f57154285a0aafb83aed68753ee3
pfarkas93 <pfarkas93@gmail.com>
2024-01-17 12:51:01 +0200

    uint8_t definition added to global types

--- src/00_GLOBALS/GLOBALS_Types.h
+++ src/00_GLOBALS/GLOBALS_Types.h
@@ -1,13 +1,12 @@
 #ifndef _GLOBALS_TYPES_H_
 #define _GLOBALS_TYPES_H_
 
-#include <math.h>
-
 // Application Type Definitions
 typedef unsigned int uint32_t;
 typedef int int32_t;
 typedef unsigned short uint16_t;
 typedef short int16_t;
+typedef unsigned char uint8_t;
 
 #ifndef __cplusplus
 typedef unsigned char bool;
