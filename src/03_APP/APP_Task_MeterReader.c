da0f29407b97a9cb3f0c293fe74c35deac6258cc
pfarkas93 <pfarkas93@gmail.com>
2024-01-18 23:09:52 +0200

    APP state machine added and implemented

--- src/03_APP/APP_Task_MeterReader.c
+++ src/03_APP/APP_Task_MeterReader.c
@@ -17,6 +17,8 @@ TESTABLE_STATIC uint8_t S_idString[ID_STRING_SIZE] = {0u};
 TESTABLE_STATIC uint16_t S_sizeOfIdStringInBytes = 0u;
 TESTABLE_STATIC uint16_t S_sizeOfMeterDataInBytes = 0u;
 
+TESTABLE_STATIC E_MeterReaderStates_t S_meterReaderTaskState = E_MeterReaderStates_Init;
+
 
 
 
@@ -27,10 +29,15 @@ TESTABLE_STATIC uint16_t S_sizeOfMeterDataInBytes = 0u;
 static void SendRequestIdToMeter(void);
 static TxDoneUartCallbackFunction_t RequestIdMessageTxDoneCallback(void);
 static TxDoneUartCallbackFunction_t AckMessageTxDoneCallback(void);
+static void ListenToMeterDataReadoutReplyOnANewBaudRate(void);
+static void ListenToMeterDeviceIdReply(void);
 static RxDoneUartCallbackFunction_t IdRxDoneCallback(uint16_t sizeOfReceivedDataInBytes);
+static void SaveIdStringToStaticVariable(void);
+static void SendAckAndDataReadoutRequestToMeter(void);
 static RxDoneUartCallbackFunction_t MeterDataRxDoneCallback(uint16_t sizeOfReceivedDataInBytes);
 static void Sleep(void);
 static void WakeUp(void);
+static void NextState(E_MeterReaderStates_t nextState);
 
 
 
@@ -44,7 +51,110 @@ void APP_Task_MeterReader(void)
     //task loop
     for(;;)
     {
-        
+        switch(S_meterReaderTaskState)
+        {
+            case E_MeterReaderStates_Init:
+            {
+                RTE_Communication_Init();
+                NextState(E_MeterReaderStates_Idle);
+                break;
+            }
+            case E_MeterReaderStates_Idle:
+            {
+                /*sleep until a trigger (button push or something) arrives and the communication can start*/
+                NextState(E_MeterReaderStates_SendRequestId);
+                break;
+            }
+            case E_MeterReaderStates_SendRequestId:
+            {
+                SendRequestIdToMeter();
+                NextState(E_MeterReaderStates_SleepUntilRequestIdTxDone);
+                Sleep();
+                break;
+            }
+            case E_MeterReaderStates_SleepUntilRequestIdTxDone:
+            {
+                /*RequestIdTxDoneCallback wakes up the task*/
+                NextState(E_MeterReaderStates_StartListeningToMeterDeviceIdReply);
+                break;
+            }
+            case E_MeterReaderStates_StartListeningToMeterDeviceIdReply:
+            {
+                ListenToMeterDeviceIdReply();
+                NextState(E_MeterReaderStates_SleepUntilMeterDeviceIdRxDone);
+                Sleep();
+                break;
+            }
+            case E_MeterReaderStates_SleepUntilMeterDeviceIdRxDone:
+            {
+                /*IdRxDoneCallback wakes up the task*/
+                NextState(E_MeterReaderStates_SaveDeviceIdAndParseBaudRate);
+                break;
+            }
+            case E_MeterReaderStates_SaveDeviceIdAndParseBaudRate:
+            {   
+                SaveIdStringToStaticVariable();
+                S_baudRateCharacterInHex = RTE_CommunicationProtocolParser_GetBaudRateValueCharacterInHexFromIdString(S_idString, S_sizeOfIdStringInBytes);
+                NextState(E_MeterReaderStates_SendAckAndDataReadoutRequest);
+                break;
+            }
+            case E_MeterReaderStates_SendAckAndDataReadoutRequest:
+            {
+                SendAckAndDataReadoutRequestToMeter();
+                NextState(E_MeterReaderStates_SleepUntilAckAndDataReadoutRequestTxDone);
+                Sleep();
+                break;
+            }
+            case E_MeterReaderStates_SleepUntilAckAndDataReadoutRequestTxDone:
+            {
+                /*AckMessageTxDoneCallback wakes up the task*/
+                NextState(E_MeterReaderStates_SetNewBaudRate);
+                break;
+            }
+            case E_MeterReaderStates_SetNewBaudRate:
+            {
+                /*Parse baud rate to numerical value*/
+                RTE_Communication_SetMeterDataRxBaudRate(S_baudRateCharacterInHex);
+                NextState(E_MeterReaderStates_StartListeningToDataReadoutReply);
+                break;
+            }
+            case E_MeterReaderStates_StartListeningToDataReadoutReply:
+            {
+                ListenToMeterDataReadoutReplyOnANewBaudRate();
+                NextState(E_MeterReaderStates_SleepUntilDataReadoutReplyRxDone);
+                Sleep();
+                break;
+            }
+            case E_MeterReaderStates_SleepUntilDataReadoutReplyRxDone:
+            {
+                /*MeterDataRxDoneCallback wakes up the task*/
+                NextState(E_MeterReaderStates_ParseAndSaveData);
+                break;
+            }
+            case E_MeterReaderStates_ParseAndSaveData:
+            {
+                /*Parse and save data*/
+                /*We have the length of read data from the callback*/
+                NextState(E_MeterReaderStates_Idle);
+                break;
+            }
+            case E_MeterReaderStates_HandleError:
+            {
+                /*Handle error*/
+                NextState(E_MeterReaderStates_Idle);
+                break;
+            }
+            case E_Count_of_MeterReaderStates:
+            {
+                NextState(E_MeterReaderStates_HandleError);
+                break;
+            }
+            default:
+            {
+                NextState(E_MeterReaderStates_HandleError);
+                break;
+            }
+        }
     }
 }
 
@@ -57,17 +167,12 @@ static void SendRequestIdToMeter(void)
     uint16_t actualSizeOfMessageData = 0u;
     RTE_CommunicationProtocolParser_FillBufferWithRequestIdMessage(SV_messageBuffer, &actualSizeOfMessageData);
     RTE_Communication_SendMessageToMeterDevice(SV_messageBuffer, actualSizeOfMessageData, RequestIdMessageTxDoneCallback);
-    Sleep();
-
 }
 
 static TxDoneUartCallbackFunction_t RequestIdMessageTxDoneCallback(void)
 {
-    /*@TODO:*/
-    // step to the next state -- set state variable
     WakeUp();
 }
-
 
 static void ListenToMeterDeviceIdReply(void)
 {
@@ -77,37 +182,34 @@ static void ListenToMeterDeviceIdReply(void)
                                                 sizeof(SV_readBuffer), 
                                                 idReplyTerminatingCharacter,
                                                 IdRxDoneCallback);
-
-    /*@TODO:*/
-    // step to the next state -- set state variable
-    Sleep();
 }
 
 static RxDoneUartCallbackFunction_t IdRxDoneCallback(uint16_t sizeOfReceivedDataInBytes)
 {
-    /*@TODO:*/
-    // step to the next state -- set state variable
     S_sizeOfIdStringInBytes = sizeOfReceivedDataInBytes;
     WakeUp();
+}
+
+static void SaveIdStringToStaticVariable(void)
+{
+    uint16_t idStringIndex = 0u;
+
+    for(idStringIndex = 0u; idStringIndex < S_sizeOfIdStringInBytes; idStringIndex++)
+    {
+        S_idString[idStringIndex] = SV_readBuffer[idStringIndex];
+    }
 }
 
 static void SendAckAndDataReadoutRequestToMeter(void)
 {
     uint16_t actualSizeOfMessageData = 0u;
-    uint8_t baudRateCharacterInHex = RTE_CommunicationProtocolParser_GetBaudRateValueCharacterInHexFromIdString(S_idString, S_sizeOfIdStringInBytes);
 
-    RTE_CommunicationProtocolParser_FillBufferWithAckAndDataReadoutRequestMessage(SV_messageBuffer, &actualSizeOfMessageData, baudRateCharacterInHex);
+    RTE_CommunicationProtocolParser_FillBufferWithAckAndDataReadoutRequestMessage(SV_messageBuffer, &actualSizeOfMessageData, S_baudRateCharacterInHex);
     RTE_Communication_SendMessageToMeterDevice(SV_messageBuffer, actualSizeOfMessageData, AckMessageTxDoneCallback);
-    
-    /*@TODO:*/
-    // step to the next state -- set state variable
-    Sleep();
 }
 
 static TxDoneUartCallbackFunction_t AckMessageTxDoneCallback(void)
 {
-    /*@TODO:*/
-    // step to the next state -- set state variable
     WakeUp();
 }
 
@@ -115,24 +217,16 @@ static void ListenToMeterDataReadoutReplyOnANewBaudRate(void)
 {
     uint8_t dataReadReplyTerminatingCharacter = RTE_CommunicationProtocolParser_GetDataReadReplyRxTerminatingCharacter();
 
-    RTE_Communication_SetMeterDataRxBaudRate(S_baudRateCharacterInHex); //it will be converted with the parser beforehand
-
     RTE_Communication_ListenToMeterDeviceAnswer(SV_readBuffer, 
                                                 sizeof(SV_readBuffer), 
                                                 dataReadReplyTerminatingCharacter,
                                                 MeterDataRxDoneCallback);
-    /*@TODO:*/
-    // step to the next state -- set state variable
-    Sleep();
 }
 
 static RxDoneUartCallbackFunction_t MeterDataRxDoneCallback(uint16_t sizeOfReceivedDataInBytes)
 {
-    /*@TODO:*/
-    // step to the next state -- set state variable
     S_sizeOfMeterDataInBytes = sizeOfReceivedDataInBytes;
     WakeUp();
-
 }
 
 static void Sleep(void)
@@ -143,5 +237,10 @@ static void Sleep(void)
 static void WakeUp(void)
 {
 
+}
+
+static void NextState(E_MeterReaderStates_t nextState)
+{
+    S_meterReaderTaskState = nextState;
 }
 
--- src/03_APP/APP_Task_MeterReader.h
+++ src/03_APP/APP_Task_MeterReader.h
@@ -5,4 +5,23 @@
 
 void APP_Task_MeterReader(void);
 
+typedef enum
+{
+    E_MeterReaderStates_Init,
+    E_MeterReaderStates_Idle,
+    E_MeterReaderStates_SendRequestId,
+    E_MeterReaderStates_SleepUntilRequestIdTxDone,
+    E_MeterReaderStates_StartListeningToMeterDeviceIdReply,
+    E_MeterReaderStates_SleepUntilMeterDeviceIdRxDone,
+    E_MeterReaderStates_SaveDeviceIdAndParseBaudRate,
+    E_MeterReaderStates_SendAckAndDataReadoutRequest,
+    E_MeterReaderStates_SleepUntilAckAndDataReadoutRequestTxDone,
+    E_MeterReaderStates_SetNewBaudRate,
+    E_MeterReaderStates_StartListeningToDataReadoutReply,
+    E_MeterReaderStates_SleepUntilDataReadoutReplyRxDone,
+    E_MeterReaderStates_ParseAndSaveData,
+    E_MeterReaderStates_HandleError,
+    E_Count_of_MeterReaderStates
+} E_MeterReaderStates_t;
+
 #endif //_APP_TASK_METERREADER_H_
